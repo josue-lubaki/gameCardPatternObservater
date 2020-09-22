e9954e73f4ad866ac5de63ac1680a0839a28f57d
Josue Lubaki <josue.lubaki@uqtr.ca>
2020-09-22 08:38:01 -0700

    new class

---
+++ Jeu_Carte/Partie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jeu_Carte
+{
+    
+    class Partie
+    {
+        //public static List<Carte> paquet = new List<Carte>();
+        
+        public Partie(int nbre_Joueur,List<Joueur> joueur) {
+            //this.nbre_Joueur = nbre_Joueur;
+            liste_Joueur = joueur;
+        }
+        public int nbre_Joueur
+        {
+            get { return nbre_Joueur; }
+        }
+
+        public static List<Joueur> liste_Joueur = new List<Joueur>();
+        public List<Joueur> Liste_Joueur{
+            get {return liste_Joueur;}
+            set { liste_Joueur = value; }
+        } 
+
+       
+    }
+}
