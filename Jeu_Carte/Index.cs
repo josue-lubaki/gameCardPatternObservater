90c707bdf0ce6f4811229c596fca920bbef71602
ismaelcoulibaly <coulibai@uqtr.ca>
2020-09-28 21:54:03 -0400

    play implementation

--- Jeu_Carte/Index.cs
+++ Jeu_Carte/Index.cs
@@ -1,25 +1,28 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 /*==================================================================================================
  *                          @Authors : Josue Lubaki & Ismael Coulibaly 
  *==================================================================================================*/
 namespace Jeu_Carte
 {
+
     class Index
     {
         public enum Color { Trefle, Carreau, Coeur, Pique };
-        private static int nbre_participant = 1;
-
+        private static int nbre_participant = 2;
+        private static bool arret = false;
+        private static int chf;
+        private static Joueur jr_debut;
+        private static Carte trouvee;
+        private static int idx;
         // Liste vide de Joueur qui nous servira d'initialisation lors de la création d'une instance Partie
         private static List<Joueur> list_jr_ = new List<Joueur>();
         private static Partie jeu;
-        
-
-
-        
+        private static Random aleatoire = new System.Random();
         static void Main(string[] args)
         {
+            Random aleatoire = new System.Random();
+
             /* Liste contenant les cartes randomisées, et faire, pour user_1 prendre les cartes de 0 à 7
             * et Pour User_2 prendre les cartes allant de 8 à 15 ainsi de suite.
             * Utilisation : retirer les 8 premières cartes pour le joueur 1 ; les 8 cartes suivantes pour le joueur 2
@@ -28,10 +31,9 @@ namespace Jeu_Carte
             * ===> (code P1) <=== ci-bas
             */
             List<Carte> liste_u = new List<Carte>();
-            
-            Console.Write("Entrer le nombre de Joueur Participant :");
-            nbre_participant = Convert.ToInt32(Console.ReadLine());
-           
+
+
+
             // Creation de 52 Cartes du Jeu
             for (int i = 1; i <= 13; i++)
             {
@@ -42,7 +44,29 @@ namespace Jeu_Carte
                 }
             }
 
-            /* La Boucle While() est censée créer 8 Cartes d'une manière aléatoire associées 
+
+
+
+            // première Carte posée sur la table de Jeu
+            int number = aleatoire.Next(0, Carte.paquet.Count);
+            Carte firstCarte = Carte.paquet[number];
+            // retirer la carte selectionnée de la liste paquet pour éviter d'avoir des doublons
+            Carte.paquet.RemoveAt(number);
+            // on pose la première carte sur la table de jeu
+            Partie.pileDepot.Add(firstCarte);
+
+
+
+            int p_index = 1;
+            while (p_index <= nbre_participant)
+            {
+                do
+                {
+                    Console.Write("Entrer le nombre de Joueur Participant (entre 2 et 4) : ");
+                    nbre_participant = Convert.ToInt32(Console.ReadLine());
+                } while (nbre_participant < 2 || nbre_participant > 4);
+
+                /* La Boucle While() est censée créer 8 Cartes d'une manière aléatoire associées 
              * à chaque Joueur(nbre_participant).
              * Ainsi donc si nous avons 2 joueurs, alors liste_u.Count = 16 Cartes puisque le while() s'éxécutera deux fois
              * Ainsi donc si nous avons 3 joueurs, alors liste_u.Count = 24 Cartes puisque le while() s'éxécutera trois fois
@@ -50,39 +74,23 @@ namespace Jeu_Carte
              * 
              *  ===> (code P2) <=== ci-bas
              */
-            int cpt = 1;//compteur permettant d'executer la boucle while le nombre de fois qu'il faut. càd autant que le nombre de participant
-            while (cpt <= nbre_participant)
-            {
-                for (int t = 0; t < 8; t++)
+                int cpt = 1;
+                while (cpt <= nbre_participant)
                 {
-                    Random aleatoire = new System.Random();
-                    Carte carte_hasard;
+                    for (int t = 0; t < 8; t++)
+                    {
+                        Carte hasard = Partie.pileDepot[Partie.pileDepot.Count-1];
+                        //Console.WriteLine(hasard);
 
-                    /* Prendre un chiffre au hasard et le faire correspondre à une carte 
-                     * sur les 52 cartes existantes
-                     */
-                    int chiffre_hasard = aleatoire.Next(0, Carte.paquet.Count);
-                    carte_hasard = Carte.paquet[chiffre_hasard];
-                    // retirer la carte selectionnée de la liste paquet pour éviter d'avoir des doublons
-                    Carte.paquet.RemoveAt(chiffre_hasard);
-                    Console.WriteLine(carte_hasard);
-
-                    /* ===> (code P1) <=== ci-haut */
-                    liste_u.Add(carte_hasard);
+                        /* ===> (code P1) <=== ci-haut */
+                        liste_u.Add(hasard);
+                    }
+                    cpt++;
                 }
-                cpt++;
-            }
 
-            // Initialisation de la Partie
-            jeu = new Partie(nbre_participant, list_jr_);
-
-            int p_index = 1;
-            while (p_index <= nbre_participant)
-            {
                 Joueur p;
-
-                for (p_index = 1; p_index <= nbre_participant; p_index++) {
-
+                for (p_index = 1; p_index <= nbre_participant; p_index++)
+                {
                     Console.Write("Entrer le Nom du Joueur " + p_index + " : ");
                     string nom_joueur = Console.ReadLine();
                     Console.Write("Entrer le Prenom du Joueur " + p_index + " : ");
@@ -91,7 +99,8 @@ namespace Jeu_Carte
                     List<Carte> list_contener = new List<Carte>();
 
                     // Condition if-else : ===> (code P2) <=== ci-haut
-                    if (liste_u.Count == 8) { // Pour 1 Joueur
+                    if (liste_u.Count == 8)
+                    { // Pour 1 Joueur
                         for (int i = 0; i < (liste_u.Count); i++)
                         {
                             list_contener.Add(liste_u[i]);
@@ -106,24 +115,26 @@ namespace Jeu_Carte
                     }
                     else if (liste_u.Count == 24) // Pour 3 joueurs
                     {
-                        for (int i = 0; i < (liste_u.Count - 16 ); i++)
+                        for (int i = 0; i < (liste_u.Count - 16); i++)
                         {
                             list_contener.Add(liste_u[i]);
                         }
                     }
                     else if (liste_u.Count == 32) // Pour 4 Joueurs
                     {
-                        for (int i = 0; i < (liste_u.Count - 24 ); i++)
+                        for (int i = 0; i < (liste_u.Count - 24); i++)
                         {
                             list_contener.Add(liste_u[i]);
                         }
                     }
                     else // Pour Autres valeurs
                     {
-                       Console.WriteLine("Désolé, le nombre des joueurs est incorrect, nous n'acceptons qu'un maximun" +
-                       "de 4 Joueurs");
+                        Console.WriteLine("Désolé, le nombre des joueurs est incorrect, nous n'acceptons qu'un maximun" +
+                        "de 4 Joueurs");
                         break;
                     }
+                    // Initialisation de la Partie
+                    jeu = new Partie(nbre_participant, list_jr_);
 
                     // Ajout du nouveau Joueur p dans la liste static des Joueurs (Liste_Joueur) de la classe Partie
                     p = new Joueur(nom_joueur, prenom_joueur, list_contener);
@@ -135,12 +146,13 @@ namespace Jeu_Carte
                     int debut = 0;
                     if (debut == 0)
                     {
-                       Console.WriteLine("suppression de 8 Cartes");
-                       liste_u.RemoveRange(0, 8);
+                        //Console.WriteLine("suppression de 8 Cartes");
+                        liste_u.RemoveRange(0, 8);
                     }
                 }
                 p_index++;
             }
+
     
             /* APPEL DE LA FONCTION AFFICHAGE */
             //affichage_liste_Joueur(jeu.Liste_Joueur);
--- Jeu_Carte/Partie.cs
+++ Jeu_Carte/Partie.cs
@@ -28,6 +28,84 @@ namespace Jeu_Carte
             set { liste_Joueur = value; }
         }
 
+
+
+       public static List<Carte> pioche = new List<Carte>();// pile de pioche
+       public static List<Carte> pileDepot = new List<Carte>();//pile de depot de l'arene
+
+
+        //METHODE QUI VA LANCER LE JEU
+        private void Play()
+        {
+            
+            //On remplit la pioche avec le reste des cartes non distribués au joueur (cartes dans le paquet)
+            Console.WriteLine("Voici le contenu de la pioche ");
+
+
+
+            /* for (int k = 0; k < Carte.paquet.Count; k++)
+               {
+                   pioche.Add(Carte.paquet[k]);
+                   Console.WriteLine($"           {pioche[k]}");
+
+               }
+               Console.WriteLine($"Il y a {pioche.Count} cartes restantes dans la pioche");*/
+            pioche = Carte.paquet;
+
+            //******** un joueur est choisi aléatoirement pour commencer la partie***********
+            //
+            //
+            //
+
+
+            Random aleatoire = new System.Random();
+            int randomIndex = aleatoire.Next(0, liste_Joueur.Count);// on choisit un index au hasard
+            Joueur randomPlayer;
+            randomPlayer = liste_Joueur[randomIndex];
+
+
+            pileDepot.Add(randomPlayer.GetList_Carte()[randomPlayer.GetList_Carte().Count - 1]);///je voudrais acceder a la liste de carte du joueur choisi aleatoirement
+            randomPlayer.GetList_Carte().RemoveAt(randomPlayer.GetList_Carte().Count - 1);//pour prendre une carte de sa main et la mettre dans la pile de depot de l'arene.
+
+
+            Boolean carteValide = false; //carte de meme couleur ou de meme valeur sur la pile de dépot
+            Boolean tourComplet = false;//variable permettant de detecter lorsque tous les joueurs ont joue (un tour complet)
+            
+            
+            for (int i= liste_Joueur.IndexOf(randomPlayer); i < liste_Joueur.Count; i++)
+            {
+                List<Carte> echantillon = new List<Carte>();//liste de carte qui permettra de conserver les cartes possibles d'etre jouees
+
+                for(int j=0; i< liste_Joueur[i].GetList_Carte().Count; j++)
+                {
+                    Carte uneCarte = liste_Joueur[i].GetList_Carte()[j];
+
+                    if(uneCarte.GetColor() == pileDepot[pileDepot.Count - 1].GetColor()
+                        || uneCarte.GetValeur_String() == pileDepot[pileDepot.Count-1].GetValeur_String())// on verifie que les conditions sont respectees
+
+                     {
+                        echantillon = new List<Carte>();
+                        echantillon.Add(uneCarte);
+                        pileDepot.Add(uneCarte);
+                    }
+                }
+
+                /*if (i == liste_Joueur.Count)
+                {
+
+                }
+                */
+                tourComplet = true;
+
+                if(i == liste_Joueur.Count - 1) { i = -1; }
+                
+            }
+
+            
+
+        }
+
+
         public override string ToString()
         {
             var output = "\t\tNotre Partie de Jeu comprend " + Nbre_Joueur + " Joueurs\n" +
@@ -35,61 +113,11 @@ namespace Jeu_Carte
             Console.WriteLine("==================================================================================");
             Console.Write(output);
             Index.affichage_liste_Joueur(Liste_Joueur);
-            return("==================================================================================");
-
-        }
-
-
-        //METHODE QUI VA LANCER LE JEU
-        public void play()
-        {
-            ToString();
-            //On remplit la pioche avec le reste des cartes non distribués au joueur (cartes dans le paquet)
-            Console.WriteLine("Voici le contenu de la pioche ");
-
-
-
-            ArrayList pioche = new ArrayList();// pile de pioche
-            ArrayList pileDepot = new ArrayList();//pile de depotde l'arene
-
-            for (int k = 0; k < Carte.paquet.Count; k++)
-            {
-                pioche.Add(Carte.paquet[k]);
-                Console.WriteLine($"        *****    {pioche[k]}");
-
-            }
-            Console.WriteLine($"Il y a {pioche.Count} cartes restantes dans la pioche");
-
-
-            //******** un joueur est choisi aléatoirement pour commencer la partie***********
-            //
-            //
-            //
-           
-
-            Random aleatoire = new System.Random();
-            int randomIndex = aleatoire.Next(0, liste_Joueur.Count);// on choisit un index au hasard
-            Joueur randomPlayer;
-            randomPlayer= liste_Joueur[randomIndex];
-
-
-            pileDepot.Add(randomPlayer.GetList_Carte().Count - 1);///je voudrais acceder a la liste de carte du joueur choisi aleatoirement
-            randomPlayer.GetList_Carte().RemoveAt(randomPlayer.GetList_Carte().Count -1);//pour prendre une carte de sa main et la mettre dans la pile de depot de l'arene.
-
-
-            for (int i=0; i < liste_Joueur.Count; i++)
-            {
-                int chiffre_hasard = aleatoire.Next(0, Carte.paquet.Count);
-            }
+            Play();
+            return ("==================================================================================");
 
         }
     }
-
-
-   /* static void Main(string[] args)
-    {
-    }
-   */
 
 
 
