70dc86a2b608c3110bb9d27fe60b353e8539c8a0
ismaelcoulibaly <coulibai@uqtr.ca>
2020-10-08 04:48:47 -0400

    cool

---
+++ Jeu_Uno/Jeu_Uno/Events/DepotEtPiocheEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+/*========================================================================== */
+/*          @Authors : Josue Lubaki & Ismael Coulibaly & Xuyao Hu           */
+/*==========================================================================*/
+namespace Jeu_Uno
+{
+    class DepotEtPiocheEvent : EventArgs
+    {
+        private LinkedListNode<Joueur> joueur;
+        public DepotEtPiocheEvent(LinkedListNode<Joueur> joueur)
+        {
+            this.joueur = joueur;
+        }
+        public LinkedListNode<Joueur> Joueur
+        {
+            get { return joueur; }
+        }
+    }
+}
